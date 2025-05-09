1403fe2339d9d4636186ca371d135468f6676a32
Hossen Yassen <103365864+HossenYassen@users.noreply.github.com>
2025-05-09 12:47:12 +0000

    Hossen solution for the rest of the questions

--- HW#3 Arrays/js.js
+++ HW#3 Arrays/js.js
@@ -96,38 +96,101 @@ console.log(oneSentence);
 *                                      מעבדים מערכים
 ************************************************************************************/
 
-//                                     Task #
+//                                     Task #14
+
+// Print the name of each student inside properate message
+const students = ["דני", "נועה", "רן", "תמר"];
+students.forEach(function(studnet){
+    console.log(`שלום, ${studnet}! בהצלחה במבחן`);
+});
+
+/************************************************************************************/
+//                                     Task #15
+
+// Create new array that contains the prices in $: 1$ = 3.5 Shekels
+const pricesILS = [100, 250, 75, 320];
+// תוצאה: [28.57, 71.43, 21.43, 91.43]
+const pricesUSD = pricesILS.map(function(price){
+    return parseFloat((price/3.5).toFixed(2));
+})
+console.log(pricesUSD);
+
+/************************************************************************************/
+//                                     Task #16
 
 
 
 /************************************************************************************/
 //                                     Task #
 
+// Filtering just the users with isLoggedIn is true
+const users = [
+{ username: "Tom", isLoggedIn: true },
+{ username: "Jerry", isLoggedIn: false },
+{ username: "Spike", isLoggedIn: true }
+];
+
+const logedin = users.filter(function(user){
+    return user["isLoggedIn"];
+})
+
+console.log(logedin);
 
 
 /************************************************************************************/
-//                                     Task #
+//                                     Task #17
 
+// Checks if there at least one product that is out of stock
+const products = [
+{ name: "Book", quantity: 3 },
+{ name: "Pen", quantity: 0 },
+{ name: "Notebook", quantity: 5 }
+];
+
+const result = products.some((prod) => prod["quantity"] === 0)
+console.log(result ? "Yes, there is a product that is out of stock" 
+                   : "No, all the products in the stock");
+
+/************************************************************************************/
+//                                     Task #18
+
+// Checks if all teh students pass the exam
+const grades = [72, 85, 90, 67, 58];
+// במקרה הזה - צריך להחזיר false
+const result = grades.every((grade) => grade >== 60);
+console.log(result ? "Yes, all the students pass" 
+                   : "No, not all the students pass");
+
+/************************************************************************************/
+//                                     Task #19
+
+// Filter the names of the employees in capital letter that their sallaries are
+// grater than 8000
+const employees = [
+  { name: "Sharon", salary: 9500 },
+  { name: "David", salary: 7800 },
+  { name: "Lior", salary: 12000 }
+];
+
+const results = employees.filter(emp => emp.salary > 8000).map(emp => emp.name.toUpperCase());
+
+console.log(results);
 
 
 /************************************************************************************/
-//                                     Task #
+//                                     Task #20
 
+// Check if there is at least one book with more than 500 pages:
+// if yes: return all the books with more than 500 pages
+const books = [
+  { title: "Book A", pages: 450 },
+  { title: "Book B", pages: 520 },
+  { title: "Book C", pages: 600 }
+];
 
-
-/************************************************************************************/
-//                                     Task #
-
-
-
-/************************************************************************************/
-//                                     Task #
-
-
-
-/************************************************************************************/
-//                                     Task #
-
-
+const results1 = books.some((book) => book["pages"] > 500) 
+  ? books.filter((book) => book["pages"] > 500)  // Use filter to return the books themselves
+  : [];
+console.log(results1);
 
 /************************************************************************************/
