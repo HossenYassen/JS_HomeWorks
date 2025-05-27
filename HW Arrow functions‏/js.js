18c4cc3d5e863fd73ac8313a3f38f5cc271f72bf
Hossen Yassen <103365864+HossenYassen@users.noreply.github.com>
2025-05-27 17:32:08 +0000

    Hossen Yassen Solution

--- HW Arrow functions‏/js.js
+++ HW Arrow functions‏/js.js
@@ -9,8 +9,29 @@
 *                                    
 ************************************************************************************/
 
-//                                     Task #
+//                                     Task #1
 
+const box = document.querySelector('#hoverBox')
+let changeColor = box => {
+    box.style.background="orange"
+}
 
+/************************************************************************************/
+
+//                                     Task #2
+
+let returnColor = box =>{
+    box.style.background="grey"
+}
+
+/************************************************************************************/
+
+//                                     Task #3
+
+const btn = document.querySelector('#clickBtn')
+let count=document.getElementById('clickCount')
+let countClick = btn =>{
+    count.textContent = parseInt(count.textContent)+1
+}
 
 /************************************************************************************/
